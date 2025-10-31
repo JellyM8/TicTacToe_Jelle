47fb27fcb5bcde01939e0ee5a010efbffff1fe30
JellyM8 <469482@student.saxion.nl>
2025-10-31 16:21:14 +0100

    juiste classes bij de functie en imports neer gezet

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 # importing van tkinter
-from tkinter import *
-from char_select import *
-from gamestate import *
-from screen import *
-from turn_board import *
+from tkinter import Tk
+from char_select import Char_select
+from gamestate import Gamestate
+from screen import Mainscreen_en_widgets
+from turn_board import Turn_board
 root = Tk()
 
 # bovenstaande werkte niet dus probeerde daarna alle aangemaakte functies op te roepen
-x_select()
-o_select()
-draw_board()
-Check_game_over()
-restart()
-Quit()
-player_positie()
-Ai_turn()
+Char_select.x_select()
+Char_select.o_select()
+Turn_board.draw_board()
+Gamestate.Check_game_over()
+Mainscreen_en_widgets.restart()
+Mainscreen_en_widgets.quit()
+Turn_board.player_positie()
+Turn_board.Ai_turn()
 root.mainloop()
