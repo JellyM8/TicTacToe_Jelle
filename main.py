63840f59783b61a1884ab1bb9181c849e95b03b3
JellyM8 <469482@student.saxion.nl>
2025-10-31 13:58:04 +0100

    update gedaan aan dat ie het gecombineerde bestand van turn en board importeert

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 # importing van tkinter
 from tkinter import *
 from char_select import *
-from board import *
 from gamestate import *
 from screen import *
-from turn import *
+from turn_cycle_board import *
 root = Tk()
 
 # bovenstaande werkte niet dus probeerde daarna alle aangemaakte functies op te roepen
@@ -13,6 +12,7 @@ o_select()
 draw_board()
 Check_game_over()
 restart()
+Quit()
 player_positie()
 Ai_turn()
 root.mainloop()
