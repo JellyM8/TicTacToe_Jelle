41e18f77954dea3d4c77886d3f5076fc119c1de8
JellyM8 <469482@student.saxion.nl>
2025-11-01 12:08:32 +0100

    code van chat toegevoegd na veel issues

--- char_select.py
+++ char_select.py
@@ -1,27 +1,14 @@
-from tkinter import Button
-from tkinter import Label
+# main.py
+# ---------------------
+# Startpunt van het programma. Maakt één Tk() venster aan en laadt het hoofdscherm.
 from tkinter import Tk
-from turn_board import Turn_board
+from screen import Mainscreen_en_widgets
+
 root = Tk()
-# player char is leeg zodat deze later gevuld kan worden
+root.title("Tic Tac Toe")
 
+# Maak hoofdscherm
+Mainscreen_en_widgets(root)
 
-class Char_select:
-    # functie om player x caan te maken
-    def x_select():
-        global player_char
-        global ai_char
-        player_char = "X"
-        ai_char = "O"
-        player_label = Label(root, text=f"You have selected {player_char}").grid(row=3, column=0, columnspan=3)
-        start_button = Button(root, text="Start the game", command=Turn_board.draw_board).grid(row=4, column=0, columnspan=3)
-
-
-    # functie om player o aan te maken
-    def o_select():
-        global player_char
-        global ai_char
-        player_char = "O"
-        ai_char = "X"
-        player_label = Label(root, text=f"You have selected {player_char}").grid(row=3, column=0, columnspan=3)
-        start_button = Button(root, text="Start the game", command=Turn_board.draw_board).grid(row=4, column=0, columnspan=3)
+# Start mainloop
+root.mainloop()
