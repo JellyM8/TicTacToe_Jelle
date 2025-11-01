11d5d51d59e3c868e711bca4216f944efece4f08
JellyM8 <469482@student.saxion.nl>
2025-11-01 12:03:00 +0100

    changed code gebasseerd chat suggesties na veel issues

--- neutral_importfile.py
+++ neutral_importfile.py
@@ -1,2 +1,7 @@
+# neutral_importfile.py
+# ---------------------
+# Dit bestand dient als neutrale opslagplaats voor globale variabelen.
+# Zo kunnen andere modules de gekozen tekens (X of O) delen zonder importproblemen.
+
 player_char = None
 ai_char = None
