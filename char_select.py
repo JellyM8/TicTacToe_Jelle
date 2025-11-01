c45678ccb509968ccff44cfefd635f82e1170474
JellyM8 <469482@student.saxion.nl>
2025-11-01 09:57:22 +0100

    juiste class name geimporteerd en voor variabel gezet

--- char_select.py
+++ char_select.py
@@ -1,7 +1,7 @@
 from tkinter import Button
 from tkinter import Label
 from tkinter import Tk
-from board import draw_board
+from turn_board import Turn_board
 root = Tk()
 # player char is leeg zodat deze later gevuld kan worden
 player_char = ""
@@ -14,7 +14,7 @@ class Char_select:
         player_char = "X"
         ai_char = "O"
         player_label = Label(root, text=f"You have selected {player_char}").grid(row=3, column=0, columnspan=3)
-        start_button = Button(root, text="Start the game", command=draw_board).grid(row=4, column=0, columnspan=3)
+        start_button = Button(root, text="Start the game", command=Turn_board.draw_board).grid(row=4, column=0, columnspan=3)
 
 
     # functie om player o aan te maken
@@ -24,4 +24,4 @@ class Char_select:
         player_char = "O"
         ai_char = "X"
         player_label = Label(root, text=f"You have selected {player_char}").grid(row=3, column=0, columnspan=3)
-        start_button = Button(root, text="Start the game", command=draw_board).grid(row=4, column=0, columnspan=3)
+        start_button = Button(root, text="Start the game", command=Turn_board.draw_board).grid(row=4, column=0, columnspan=3)
