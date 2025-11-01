82a1df8a11ef85a46297c6eab6607ae3bd62f390
JellyM8 <469482@student.saxion.nl>
2025-11-01 10:41:04 +0100

    changed some variables and created /changed classes location to make the starting screen work

--- screen.py
+++ screen.py
@@ -2,15 +2,24 @@ from tkinter import Tk
 from tkinter import Label
 from tkinter import Button
 import os
-from char_select import Char_select
+# from char_select import Char_select
 root = Tk()
 
 class Mainscreen_en_widgets:
     # button functies op het scherm
-    main_label = Label(root, text="Welcome to Tic Tac Toe!")  # Welkom scherm
-    player_select_label = Label(text="Select a player to play as")  # Kies een speler
-    x_button = Button(root, text="player X", command=Char_select.x_select)  # button om X te zijn
-    o_button = Button(root, text="player O", command=Char_select.o_select)  # button om O te zijn
+    def __init__(self, master):
+        self.grid(sticky= "nsew")
+        # self.x_button
+        # self.o_button
+    # scherm zelf aanmaken aanmaken
+        main_label.grid(row=0, column=0, columnspan=3)
+        player_select_label.grid(row=1, column=0, columnspan=3)
+        x_button.grid(row=2, column=0, sticky="ew")
+        o_button.grid(row=2, column=1, sticky="ew")
+        main_label = Label(root, text="Welcome to Tic Tac Toe!")  # Welkom scherm
+        player_select_label = Label(text="Select a player to play as")  # Kies een speler
+        x_button = Button(root, text="player X")# button om X te zijn
+        o_button = Button(root, text="player O")# button om O te zijn
 
 
 
@@ -26,8 +35,3 @@ class Mainscreen_en_widgets:
     Button(root, text="Quit", command=quit).grid(row=10, column=0, columnspan=3)
 
 
-    # scherm zelf aanmaken aanmaken
-    main_label.grid(row=0, column=0, columnspan=3)
-    player_select_label.grid(row=1, column=0, columnspan=3)
-    x_button.grid(row=2, column=0, sticky="ew")
-    o_button.grid(row=2, column=1, sticky="ew")
