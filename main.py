1320cdd1eaf94ba1fe2b9a6c8430ea399d63e927
JellyM8 <469482@student.saxion.nl>
2025-11-02 10:52:35 +0100

    code linting gedaan, black isort mypy en ruff

--- main.py
+++ main.py
@@ -2,13 +2,16 @@
 # ---------------------
 # Startpunt van het programma. Maakt één Tk() venster aan en laadt het hoofdscherm.
 from tkinter import Tk
+
 from screen import Mainscreen_en_widgets
+
 
 def main():
     root = Tk()
     root.title("Tic Tac Toe")
     app = Mainscreen_en_widgets(root)
     root.mainloop()
+
 
 if __name__ == "__main__":
     main()
