754072757975c24211e7c65e42cc1943221cb562
JellyM8 <469482@student.saxion.nl>
2025-11-02 10:54:22 +0100

    code linting gedaan black isort mypy ruff

--- char_select.py
+++ char_select.py
@@ -2,8 +2,10 @@
 # ---------------------
 # Laat de speler kiezen tussen X of O, en start daarna het bord.
 from tkinter import Button, Label
+
 import neutral_importfile as n
 from turn_board import Turn_board
+
 
 class Char_select:
     def __init__(self, root):
@@ -13,13 +15,17 @@ class Char_select:
         n.player_char = "X"
         n.ai_char = "O"
         Label(self.root, text="You are X").grid(row=3, column=0, columnspan=3)
-        Button(self.root, text="Start Game", command=self.start_game).grid(row=4, column=0, columnspan=3)
+        Button(self.root, text="Start Game", command=self.start_game).grid(
+            row=4, column=0, columnspan=3
+        )
 
     def o_select(self):
         n.player_char = "O"
         n.ai_char = "X"
         Label(self.root, text="You are O").grid(row=3, column=0, columnspan=3)
-        Button(self.root, text="Start Game", command=self.start_game).grid(row=4, column=0, columnspan=3)
+        Button(self.root, text="Start Game", command=self.start_game).grid(
+            row=4, column=0, columnspan=3
+        )
 
     def start_game(self):
         board = Turn_board()
